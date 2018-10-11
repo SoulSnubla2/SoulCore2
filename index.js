a0535a417f5d409c169450a27cf7904b29d9d8de
SoulSnubla2 <44069945+SoulSnubla2@users.noreply.github.com>
2018-10-11 21:43:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const Discord = require('discord.js');
 const bot = new Commando.Client();
 const TOKEN = "null";
 
-bot.registry.registerGroup('simple', 'Simple');
-bot.registry.registerGroup('music', 'Music');
-bot.registry.registerGroup('rep', 'Rep')
-bot.registry.registerDefaults();
-bot.registry.registerCommandsIn(__dirname + '/commands');
+//bot.registry.registerGroup('simple', 'Simple');
+//bot.registry.registerGroup('music', 'Music');
+//bot.registry.registerGroup('rep', 'Rep')
+//bot.registry.registerDefaults();
+//bot.registry.registerCommandsIn(__dirname + '/commands');
 
 global.servers = {};
 
